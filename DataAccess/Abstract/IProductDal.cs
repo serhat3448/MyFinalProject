82a274ff78ead248c7cec10af10d9e67f57f6cf7
serhat doğru <dogru.serhat@hotmail.com>
2021-01-31 00:06:02 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Interface metotları default olarak publictir ama kendisi default olarak public değildir.
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        List<Product> GetAllByCategory(int categoryId);
+    }
+}
