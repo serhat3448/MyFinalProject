219f2cc085d2876b04d0a245db1f7698de2e4d0e
serhat doğru <dogru.serhat@hotmail.com>
2021-02-08 19:22:46 +0300

    9. Gün Sonu

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,16 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic constraint
     //class : Referans tip
     //IEntity : IEntitiy olabilir veya IEntity implemente eden bir nesne olabilir.
     //new() : new'lenebilir olmalı
+    //filter=null filtresiz de olabilir, sadece filter var iste mutlaka filtre olmalıdır.
     public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter=null);
